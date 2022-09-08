24173c1bb6f9ac2bac4c1a1fa636f80eaa2fe0c5
Hakimi Mdnoor <hakimimdnoor90@gmail.com>
2022-09-08 17:04:39 +0800

    fix landing page

--- lib/widget/splashscreen.dart
+++ lib/widget/splashscreen.dart
@@ -18,85 +18,49 @@ class _SplashScreenState extends State<SplashScreen> {
     final screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       body: screenWidth < 576
-      ? Column(children: <Widget>[
-        Expanded(
-          child: Container(
-            decoration: const BoxDecoration(
-                image: DecorationImage(
-              fit: BoxFit.cover,
-              image: AssetImage("assets/splashscreen3.gif"),
-            )),
-            child: Column(
-              children: <Widget>[
-                // Container(
-                //   padding: const EdgeInsets.fromLTRB(0, 50.0, 30, 0),
-                //   //todo: insert text
-                //   child: const Text(
-                //     'Tamadun.',
-                //     style: TextStyle(
-                //       fontSize: 64,
-                //       color: Colors.white,
-                //       fontFamily: 'MontserratExtraBold',
-                //     ),
-                //   ),
-                // ),
-                // const Text(
-                //   "lingua revolution & human civilization",
-                //   style: TextStyle(
-                //     fontSize: 16,
-                //     color: Colors.white,
-                //     fontFamily: 'PoppinsMedium',
-                //   ),
-                // ),
-                Expanded(
-                  child: Bottom(),
-                )
-              ],
-            ),
-          ),
-        ),
-      ])
-      //todo: tab
+          ? Column(children: <Widget>[
+              Expanded(
+                child: Container(
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage("assets/splashscreen3.gif"),
+                  )),
+                ),
+              ),
+            ])
+          //todo: tab
           : screenWidth < 992
-      ? Column(children: <Widget>[
-        Expanded(
-          child: Container(
-            decoration: const BoxDecoration(
-                image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: AssetImage("assets/splashscreen3.gif"),
-                )),
-            child: Column(
-              children: <Widget>[
-                // Container(
-                //   padding: const EdgeInsets.fromLTRB(0, 50.0, 30, 0),
-                //   //todo: insert text
-                //   child: const Text(
-                //     'Tamadun.',
-                //     style: TextStyle(
-                //       fontSize: 64,
-                //       color: Colors.white,
-                //       fontFamily: 'MontserratExtraBold',
-                //     ),
-                //   ),
-                // ),
-                // const Text(
-                //   "lingua revolution & human civilization",
-                //   style: TextStyle(
-                //     fontSize: 16,
-                //     color: Colors.white,
-                //     fontFamily: 'PoppinsMedium',
-                //   ),
-                // ),
-                Expanded(
-                  child: BottomTab(),
-                )
-              ],
-            ),
-          ),
+              ? Column(children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      decoration: const BoxDecoration(
+                          image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: AssetImage("assets/splashscreen3.gif"),
+                      )),
+                    ),
+                  ),
+                ])
+              : null,
+      floatingActionButton: FloatingActionButton.extended(
+        backgroundColor: mPrimaryColor,
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const LoginScreen(),
+              ));
+        },
+        label: Text(
+          'Explore',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              color: Color(hexColor('24346C')),
+              fontSize: 20,
+              fontFamily: 'MontserratBold'),
         ),
-      ])
-          : null
+      ),
     );
   }
 }
@@ -135,7 +99,7 @@ class BottomTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Align(
-      alignment: Alignment.bottomRight,
+        alignment: Alignment.bottomRight,
         child: MaterialButton(
           height: 60.0,
           minWidth: 160.0,
@@ -159,29 +123,29 @@ class BottomTab extends StatelessWidget {
                 ));
           },
         )
-      // child: RaisedButton(
-      //   padding: EdgeInsets.fromLTRB(20, 7, 20, 70),
-      //   onPressed: () {
-      //     Navigator.push(
-      //         context,
-      //         MaterialPageRoute(
-      //           builder: (context) => const LoginScreen(),
-      //         ));
-      //   },
-      //   shape: const RoundedRectangleBorder(
-      //       borderRadius: BorderRadius.all(Radius.circular(32))),
-      //   color: mPrimaryColor,
-      //   elevation: 5,
-      //   child: Text(
-      //     'Explore',
-      //     textAlign: TextAlign.center,
-      //     style: TextStyle(
-      //         color: Color(hexColor('24346C')),
-      //         fontSize: 20,
-      //         fontFamily: 'MontserratBold'),
-      //   ),
-      // ),
-    );
+        // child: RaisedButton(
+        //   padding: EdgeInsets.fromLTRB(20, 7, 20, 70),
+        //   onPressed: () {
+        //     Navigator.push(
+        //         context,
+        //         MaterialPageRoute(
+        //           builder: (context) => const LoginScreen(),
+        //         ));
+        //   },
+        //   shape: const RoundedRectangleBorder(
+        //       borderRadius: BorderRadius.all(Radius.circular(32))),
+        //   color: mPrimaryColor,
+        //   elevation: 5,
+        //   child: Text(
+        //     'Explore',
+        //     textAlign: TextAlign.center,
+        //     style: TextStyle(
+        //         color: Color(hexColor('24346C')),
+        //         fontSize: 20,
+        //         fontFamily: 'MontserratBold'),
+        //   ),
+        // ),
+        );
   }
 }
 
